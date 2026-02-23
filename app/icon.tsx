93e3e5ded0509c106ea7f42e742e2b0bc0465abc
Steven Mathew <mathewsteven92@gmail.com>
2026-02-23 10:25:16 -0500

    Mahacura

--- app/icon.tsx
+++ app/icon.tsx
@@ -14,7 +14,7 @@ export const contentType = 'image/png'
 export default function Icon() {
     return new ImageResponse(
         (
-            // ImageResponse JSX element
+
             <div
                 style={{
                     fontSize: 24,
